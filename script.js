790cb0e5d6ebf15386e32aadcb0b669aa8e3d615
sanay uj <sanayuj@Sanays-MacBook.local>
2022-08-21 12:51:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            email:{
+                required:true,
+               
+                
+            },
+            password:{
+                required:true,
+                minlength:8,
+                maxlength:10,
+            },
+            day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            year:{
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        }
+    })
+})
